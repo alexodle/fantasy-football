382afe3fd85d87398e8d039645e2cd524a7b279c
Alex Odle <aodle@palantir.com>
2015-11-05 20:47:51 -0500

    reflux hello world

--- client/app/js/App.jsx
+++ client/app/js/App.jsx
@@ -1,7 +1,25 @@
+import Actions from './actions/Actions';
+import HelloWorldStore from './stores/HelloWorldStore';
 import React from 'react';
+import Reflux from 'reflux';
 
-export default React.createClass({
+const App = React.createClass({
+
+  mixins: [Reflux.connect(HelloWorldStore)],
+
   render() {
-    return (<div>Hello World!</div>);
+    return (
+      <div>
+        <p>Text: {this.state.text}</p>
+        <p><button type='button' onClick={this.onClick}>Say hello</button></p>
+      </div>
+    );
+  },
+
+  onClick() {
+    Actions.sayHello();
   }
+
 });
+
+export default App;
---
+++ client/app/js/actions/Actions.js
@@ -0,0 +1,7 @@
+import Reflux from 'reflux';
+
+const Actions = Reflux.createActions([
+  "sayHello"
+]);
+
+export default Actions;
