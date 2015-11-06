1bb6ba60f4a0f758310c32528ac5d4a80bc2132c
Alex Odle <aodle@palantir.com>
2015-11-06 12:59:01 -0500

    dev server returns files from dir structure

--- client/devServer.js
+++ client/devServer.js
@@ -1,12 +1,37 @@
+var express = require('express');
+var fs = require("fs");
 var path = require('path');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
 var WebpackDevServer = require('webpack-dev-server');
 
+var FILES_DIR = path.resolve('./test/mockserver');
+
+var expressApp = express();
+
 var compiler = webpack(webpackConfig);
-var server = new WebpackDevServer(compiler, {
+var webpackDevServer = new WebpackDevServer(compiler, {
   contentBase: path.resolve('./app/views/'),
-  publicPath: '/dist/'
+  publicPath: '/dist/',
+  proxy: { '/api/*': 'http://localhost:8081/' }
 });
 
-server.listen(8080, 'localhost', function() {});
+expressApp.get('/api/*', function(req, res) {
+  var reqPath = req.path.substring(1);
+
+  var filePath = path.join(FILES_DIR, reqPath) + '.json';
+  var dataType = path.basename(filePath, '.json');
+
+  fs.readFile(filePath, 'utf8', function (err, data) {
+    if (err) {
+      res.sendStatus(404);
+      return;
+    }
+    var json = {};
+    json[dataType] = JSON.parse(data);
+    res.json(json);
+  });
+});
+
+webpackDevServer.listen(8080, 'localhost', function() {});
+expressApp.listen(8081, function () {});
