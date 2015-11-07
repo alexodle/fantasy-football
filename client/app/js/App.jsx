62386d6ed90978b0f77d4a912193a9097b261df0
Alex Odle <aodle@palantir.com>
2015-11-07 09:50:55 -0500

    remove hello world

--- client/app/js/App.jsx
+++ client/app/js/App.jsx
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import Actions from './actions/Actions';
 import AjaxComponent from './components/AjaxComponent';
 import FootballPlayerStore from './stores/FootballPlayerStore';
-import HelloWorldStore from './stores/HelloWorldStore';
 import React from 'react';
 import Reflux from 'reflux';
 
@@ -11,18 +10,14 @@ const POSITON_DISPLAY_ORDER = ['QB', 'RB', 'WR', 'TE', 'K']
 const App = React.createClass({
 
   mixins: [
-    Reflux.connect(HelloWorldStore, 'helloWorld'),
     Reflux.connect(FootballPlayerStore, 'footballPlayers')
   ],
 
   render() {
-    const {helloWorld, footballPlayers} = this.state;
+    const {footballPlayers} = this.state;
     const playersByPosition = _.groupBy(footballPlayers.footballPlayers, 'position');
     return (
       <div>
-        <p>Text: {helloWorld.text}</p>
-        <p><button type='button' onClick={this.onClick}>Say hello</button></p>
-        <br/>
         <AjaxComponent loadingState={footballPlayers.footballPlayers}>
           <div>
             {_.map(POSITON_DISPLAY_ORDER, function (pos) {
