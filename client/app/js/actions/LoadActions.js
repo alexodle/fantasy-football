790aceadd677e7c3dc7f49bf14b435708b4df011
Alex Odle <aodle@palantir.com>
2015-11-09 09:49:59 -0500

    LoadActions.loadMyLeagues

--- client/app/js/actions/LoadActions.js
+++ client/app/js/actions/LoadActions.js
@@ -1,13 +1,20 @@
+import _ from 'lodash';
 import Reflux from 'reflux';
 import request from 'superagent';
 
 const LoadActions = Reflux.createActions({
   loadUser: { asyncResult: true },
-  loadLeague: { asyncResult: true },
+  loadMyLeagues: { asyncResult: true },
+  loadFantasyPlayers: { asyncResult: true },
   loadDraftOrder: { asyncResult: true },
   loadDraftPicks: { asyncResult: true },
   loadFootballPlayers: { asyncResult: true }
 });
+
+function parseLeague(league) {
+  league.draft_start_date = new Date(league.draft_start_date);
+  return league;
+}
 
 LoadActions.loadUser.listen(function () {
   request
@@ -19,13 +26,25 @@ LoadActions.loadUser.listen(function () {
     });
 });
 
-LoadActions.loadLeague.listen(function () {
+LoadActions.loadMyLeagues.listen(function () {
+  // Load all leagues for which I am a member
   request
-    .get('/api/league/1/')
+    .get('/api/user/fantasy_leagues/')
     .set('Accept', 'application/json')
     .end((err, res) =>  {
         if (err) { this.failed(err); return; }
-        this.completed(res.body.league);
+        const parsedLeagues = _.map(res.body.fantasy_leagues, parseLeague);
+        this.completed(parsedLeagues);
+    });
+});
+
+LoadActions.loadFantasyPlayers.listen(function () {
+  request
+    .get('/api/league/1/fantasy_players/')
+    .set('Accept', 'application/json')
+    .end((err, res) =>  {
+        if (err) { this.failed(err); return; }
+        this.completed(res.body.fantasy_players);
     });
 });
 
