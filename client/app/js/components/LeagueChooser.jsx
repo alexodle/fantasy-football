11308b64760720a6142d85c9739595d487ce442e
Alex Odle <aodle@palantir.com>
2015-11-12 23:37:22 -0500

    league list

---
+++ client/app/js/components/LeagueChooser.jsx
@@ -0,0 +1,68 @@
+import _ from 'lodash';
+import React, {PropTypes} from 'react';
+import {loadMyLeagues, loadUser} from '../actions/LoadActions';
+import {hasLoaded} from '../utils/loadingUtils';
+import {connect} from 'react-redux';
+import Loading from './Loading';
+import FFPanel from './FFPanel';
+import {ModelShapes} from '../Constants';
+
+const LeagueChooser = React.createClass({
+
+  displayName: 'LeagueChooser',
+
+  propTypes: {
+    currentUser: ModelShapes.User,
+    dispatch: PropTypes.func.isRequired,
+    loaded: PropTypes.bool.isRequired,
+    myLeagues: PropTypes.objectOf(ModelShapes.FantasyLeague)
+  },
+
+  componentWillMount() {
+    const {dispatch} = this.props;
+    dispatch(loadMyLeagues());
+    dispatch(loadUser());
+  },
+
+  render() {
+    const {loaded, myLeagues} = this.props;
+    const sortedLeagues = _.sortBy(myLeagues, 'name');
+    return (
+      <FFPanel title='My leagues'>
+        {!loaded ? <Loading /> :
+          <ul>
+            {_.map(sortedLeagues, function (league) {
+              return (
+                <li key={league.id}>
+                  {league.name}
+                </li>
+              );
+            })}
+          </ul>
+        }
+      </FFPanel>
+    );
+  }
+
+});
+
+function selectState(state) {
+  const loaded = (
+    hasLoaded(state.meta.current_user) &&
+    hasLoaded(state.meta.my_leagues)
+  );
+
+  let myLeagues, currentUser;
+  if (loaded) {
+    myLeagues = _.pick(state.entities.fantasy_leagues, state.meta.my_leagues.items);
+    currentUser = state.entities.users[state.meta.current_user];
+  }
+
+  return {
+    currentUser,
+    loaded,
+    myLeagues
+  };
+}
+
+export default connect(selectState)(LeagueChooser);
--- client/app/js/routes.jsx
+++ client/app/js/routes.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Route, Router} from 'react-router';
+import {Route, Router, IndexRoute} from 'react-router';
 import App from './App';
 import Draft from './components/draft/Draft';
+import LeagueChooser from './components/LeagueChooser';
 
 // App Routes
 export default (
   <Router>
     <Route path="/" component={App}>
+      <IndexRoute component={LeagueChooser} />
       <Route path="draft" component={Draft} />
     </Route>
   </Router>
