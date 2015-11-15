31e220da23367656aed60f9dea400ef95c990729
Alex Odle <aodle@palantir.com>
2015-11-15 18:14:50 -0500

    some dumb draft order ui updates

--- client/app/js/components/draft/DraftOrderView.jsx
+++ client/app/js/components/draft/DraftOrderView.jsx
@@ -2,6 +2,9 @@ import _ from 'lodash';
 import React, {PropTypes} from 'react';
 import {ModelShapes} from '../../Constants';
 
+const PICKS_BEFORE = 3;
+const PICKS_AFTER = 9;
+
 export default React.createClass({
 
   displayName: 'DraftOrderView',
@@ -14,16 +17,18 @@ export default React.createClass({
 
   render() {
     const {currentDraftOrder, draftOrder, userLookup} = this.props;
+    const currentPickNumber = currentDraftOrder.order;
+    const lowIndex = Math.max(currentPickNumber - PICKS_BEFORE, 0);
+    const highIndex = Math.min(currentPickNumber + PICKS_AFTER, draftOrder.length);
+    const slice = _.slice(draftOrder, lowIndex, highIndex);
     return (
       <ul className='list-inline list-group'>
-        {_.map(draftOrder, function (o) {
+        {_.map(slice, function (o) {
+          const isActive = currentDraftOrder.order === o.order;
           return (
             <li
                 key={o.order}
-                className={
-                  'list-group-item' +
-                  (currentDraftOrder.order === o.order ? ' active' : '')
-                }
+                className={'list-group-item' + (isActive ? ' active' : '')}
             >
               (#{o.order + 1}) {userLookup[o.user_id].name}
             </li>
