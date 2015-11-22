1d08e1fa5dd5c3cbd871099e6a64a09a81973b79
Alex Odle <aodle@palantir.com>
2015-11-22 08:44:39 -0500

    more draft selector tests

--- client/test/testDraftSelectors.js
+++ client/test/testDraftSelectors.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import chai from 'chai';
-import {selectIneligibleDraftPositions} from '../app/js/selectors/draftSelectors';
+import {selectIneligibleDraftPositions, selectDraftableFootballPlayers} from '../app/js/selectors/draftSelectors';
 import {Positions} from '../app/js/Constants';
 import {newDraftPick, newFootballPlayer} from './testUtils';
 
@@ -29,13 +29,18 @@ const BASE_STATE_DO_NOT_USE_DIRECTLY = {
         teams: [1],
         rules: {
           team_reqs: TEAM_REQS,
-          max_team_size: 10
+          max_team_size: 10 // MAX_BENCH_SIZE === 1
         }
       }
     },
     football_players: _.indexBy([
       newFootballPlayer({ id: 1, pos: Positions.RB }),
-      newFootballPlayer({ id: 2, pos: Positions.RB })
+      newFootballPlayer({ id: 2, pos: Positions.RB }),
+      newFootballPlayer({ id: 3, pos: Positions.RB }),
+      newFootballPlayer({ id: 4, pos: Positions.RB }),
+      newFootballPlayer({ id: 5, pos: Positions.TE }),
+      newFootballPlayer({ id: 6, pos: Positions.QB }),
+      newFootballPlayer({ id: 7, pos: Positions.QB })
     ], 'id'),
     drafts: {
       1: {
@@ -54,7 +59,7 @@ const BASE_STATE_DO_NOT_USE_DIRECTLY = {
         draft: { order: SUCCEEDED_META, picks: SUCCEEDED_META },
         fantasy_players: { ...SUCCEEDED_META, items: [1] },
         fantasy_teams: { ...SUCCEEDED_META, items: [1] },
-        football_players: { ...SUCCEEDED_META, items: [1, 2] }
+        football_players: { ...SUCCEEDED_META, items: [1, 2, 3, 4, 5, 6, 7] }
       }
     }
   },
@@ -77,34 +82,49 @@ describe('draftSelectors', () => {
     });
 
     it('keeps flex positions open when bench is full, if flex is open', () => {
-      _.merge(state.entities.football_players, _.indexBy([
-        newFootballPlayer({ id: 3, pos: Positions.QB }),
-        newFootballPlayer({ id: 4, pos: Positions.QB })
-      ], 'id'));
       state.entities.drafts[1].picks.push(
-        newDraftPick({ fpId: 3, pickNumber: 3 }),
-        newDraftPick({ fpId: 4, pickNumber: 4 })
+        newDraftPick({ fpId: 6, pickNumber: 3 }), // QB
+        newDraftPick({ fpId: 7, pickNumber: 4 }) // QB
       );
-      state.meta.fantasy_leagues[1].football_players.items.push(3, 4);
       selectIneligibleDraftPositions(state).should.eql([Positions.QB]);
     });
 
     it('removes positions when flex and bench are full', () => {
-      _.merge(state.entities.football_players, _.indexBy([
-        newFootballPlayer({ id: 3, pos: Positions.RB }),
-        newFootballPlayer({ id: 4, pos: Positions.RB }),
-        newFootballPlayer({ id: 5, pos: Positions.TE })
-      ], 'id'));
       state.entities.drafts[1].picks.push(
-        newDraftPick({ fpId: 3, pickNumber: 3 }),
-        newDraftPick({ fpId: 4, pickNumber: 4 }),
-        newDraftPick({ fpId: 5, pickNumber: 5 })
+        newDraftPick({ fpId: 3, pickNumber: 3 }), // RB
+        newDraftPick({ fpId: 4, pickNumber: 4 }), // RB
+        newDraftPick({ fpId: 5, pickNumber: 5 }) // TE
       );
-      state.meta.fantasy_leagues[1].football_players.items.push(3, 4, 5);
       selectIneligibleDraftPositions(state).should.eql([
         Positions.RB,
         Positions.TE,
         Positions.FLEX
+      ]);
+    });
+
+  });
+
+  describe('selectDraftableFootballPlayers', () => {
+
+    it('removes drafted players from the list', () => {
+      const fbPlayers = state.entities.football_players;
+      selectDraftableFootballPlayers(state).should.eql([
+        fbPlayers[3], fbPlayers[4], fbPlayers[5], fbPlayers[6], fbPlayers[7]
+      ]);
+    });
+
+    it('removes ineligible players from the list', () => {
+      const fbPlayers = state.entities.football_players;
+
+      // Force RB, TE, and FLEX to be ineligible
+      state.entities.drafts[1].picks.push(
+        newDraftPick({ fpId: 3, pickNumber: 3 }), // RB
+        newDraftPick({ fpId: 4, pickNumber: 4 }), // RB
+        newDraftPick({ fpId: 5, pickNumber: 5 }) // TE
+      );
+
+      selectDraftableFootballPlayers(state).should.eql([
+        fbPlayers[6], fbPlayers[7]
       ]);
     });
 
