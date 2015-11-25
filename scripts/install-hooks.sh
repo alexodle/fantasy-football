dfe336396df9255b2dcb266158a5e9219b44a154
Alex Odle <aodle@palantir.com>
2015-11-25 13:54:58 -0500

    fix install-hooks paths

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 
 BASEDIR=$(dirname $0)/..
-ln -sf $BASEDIR/pre-push.sh $BASEDIR/.git/hooks/pre-push
+mv $BASEDIR/.git/hooks/pre-push  $BASEDIR/.git/hooks/pre-push.bkp
+ln -s ../../githooks/pre-push.sh $BASEDIR/.git/hooks/pre-push
