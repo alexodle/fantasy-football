d89d00b041e0ba711e6b68ffc95c7bb0a38aff13
Alex Odle <aodle@palantir.com>
2015-11-26 10:14:45 -0500

    only backup push hook if it already exists

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -e
 
 BASEDIR=$(dirname $0)/..
-mv $BASEDIR/.git/hooks/pre-push  $BASEDIR/.git/hooks/pre-push.bkp
-ln -s ../../githooks/pre-push.sh $BASEDIR/.git/hooks/pre-push
+push_hook=$BASEDIR/.git/hooks/pre-push
+
+# Backup existing push hook
+if [ -f $push_hook ]; then
+  mv $push_hook ${push_hook}.bkp
+fi
+
+ln -s ../../githooks/pre-push.sh $push_hook
