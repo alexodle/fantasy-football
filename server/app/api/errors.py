c02138df36d7c3963a881d238c452bb3f6b26393
Salim Hamed <salimhamed@gmail.com>
2015-12-19 12:36:09 -0800

    made sure all errors will return json responses to the client

--- server/app/api/errors.py
+++ server/app/api/errors.py
@@ -17,10 +17,9 @@ def bad_request(message):
 
 def unauthorized(message):
     response = jsonify({'error': 'unauthorized', 'message': message})
-
-    # HACK: should be 401, but this prevents the browser from showing the auth popup
+    # HACK: should be 401, but this prevents the
+    # browser from showing the auth popup
     response.status_code = 403
-
     return response
 
 
@@ -30,10 +29,33 @@ def forbidden(message):
     return response
 
 
-@api.errorhandler(ValidationError)
+def internal_server_error():
+    response = jsonify({'error': 'internal server error',
+                        'message': 'internal server error'})
+    response.status_code = 500
+    return response
+
+
+@api.app_errorhandler(ValidationError)
 def validation_error(e):
     """
     Error handler that's called when views raise the custom 'ValidationError'
     exception.
     """
     return bad_request(e.args[0])
+
+
+@api.app_errorhandler(404)
+def resource_not_found_error(e):
+    """
+    Error handler that's called when resource is not found.
+    """
+    return resource_not_found(e.description)
+
+
+@api.app_errorhandler(500)
+def internal_server_error_handler(e):
+    """
+    Error handler that's called when a 500 error is thrown.
+    """
+    return internal_server_error()
