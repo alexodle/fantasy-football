9106d6a78b5e7a3e3f9db2a714ba969a44c35b4b
Alex Odle <aodle@palantir.com>
2015-12-23 23:15:37 -0500

    separate raw and parsed data

--- data/scripts/run-score-update.sh
+++ data/scripts/run-score-update.sh
@@ -16,23 +16,24 @@ date=$(date +"%m%d%Y-%H%M%S")
 
 echo "START: ${date}"
 
-data_dir=$stash_dir/$date
-mkdir -p $data_dir
+data_dir=$stash_dir/$date; mkdir -p $data_dir
+raw_dir=$data_dir/raw; mkdir $raw_dir
+parsed_dir=$data_dir/parsed; mkdir $parsed_dir
 
 # Download html file with all the box scores links
-box_score_links_html=$data_dir/box_score_links.html
+box_score_links_html=$raw_dir/box_score_links.html
 wget -O $box_score_links_html http://www.sports-reference.com/cfb/years/2015-schedule.html
 
 # box score links html -> box score links json
-box_score_links_json=$data_dir/box_score_links.json
+box_score_links_json=$parsed_dir/box_score_links.json
 node ./parseBoxScoreLinksHtml.js -i $box_score_links_html -o $box_score_links_json -b $BOX_SCORE_BASE_URL
 
 # download box score html files
-box_score_html_dir=$data_dir/box_score_html
+box_score_html_dir=$raw_dir/box_score_html
 node ./downloadBoxScoresHtml.js -i $box_score_links_json -d $box_score_html_dir -w $week -c $config
 
 # parse box scores
-fb_player_stats_json=$data_dir/fb_player_stats.json
+fb_player_stats_json=$parsed_dir/fb_player_stats.json
 node ./parseBoxScoreHtml.js -d $box_score_html_dir -o $fb_player_stats_json
 
 # TODO: Push player stats to postgres
