f53681ab0943f14d23ba2f3c7a94db8960854de3
Alex Odle <aodle@palantir.com>
2015-12-23 21:14:45 -0500

    rushing/receiving stats

--- data/parseBoxScoreHtml.js
+++ data/parseBoxScoreHtml.js
@@ -17,28 +17,75 @@ const PASSING_ROW = {
   INTS: 9
 };
 
-const DEFAULT = {
+const RUSH_ROW = {
+  ATT: 2,
+  YDS: 3,
+  TDS: 5
+};
+
+const REC_ROW = {
+  REC: 6,
+  YDS: 7,
+  TDS: 9
+};
+
+const DEFAULTS = {
   // Passing
   pass_completions: 0,
   pass_attempts: 0,
   pass_yds: 0,
   pass_tds: 0,
-  pass_ints: 0
+  pass_ints: 0,
+
+  // Rushing
+  rush_attempts: 0,
+  rush_yds: 0,
+  rush_tds: 0,
+
+  // Receiving
+  rec_rec: 0,
+  rec_yds: 0,
+  rec_tds: 0
 };
+
+function parseStatInt(str) {
+  return _.isEmpty(str) ? 0 : _.parseInt(str);
+}
 
 function parsePassingRow($, $row) {
   const tds = $('td', $row).get();
-  const pass_completions = _.parseInt($(tds[PASSING_ROW.CMP]).text());
-  const pass_attempts = _.parseInt($(tds[PASSING_ROW.ATT]).text());
-  const pass_yds = _.parseInt($(tds[PASSING_ROW.YDS]).text());
-  const pass_tds = _.parseInt($(tds[PASSING_ROW.TDS]).text());
-  const pass_ints = _.parseInt($(tds[PASSING_ROW.INTS]).text());
+  const pass_completions = parseStatInt($(tds[PASSING_ROW.CMP]).text());
+  const pass_attempts = parseStatInt($(tds[PASSING_ROW.ATT]).text());
+  const pass_yds = parseStatInt($(tds[PASSING_ROW.YDS]).text());
+  const pass_tds = parseStatInt($(tds[PASSING_ROW.TDS]).text());
+  const pass_ints = parseStatInt($(tds[PASSING_ROW.INTS]).text());
   return {
     pass_completions,
     pass_attempts,
     pass_yds,
     pass_tds,
     pass_ints
+  };
+}
+
+function parseRushRecRow($, $row) {
+  const tds = $('td', $row).get();
+
+  const rush_attempts = parseStatInt($(tds[RUSH_ROW.ATT]).text());
+  const rush_yds = parseStatInt($(tds[RUSH_ROW.YDS]).text());
+  const rush_tds = parseStatInt($(tds[RUSH_ROW.TDS]).text());
+
+  const rec_rec = parseStatInt($(tds[REC_ROW.REC]).text());
+  const rec_yds = parseStatInt($(tds[REC_ROW.YDS]).text());
+  const rec_tds = parseStatInt($(tds[REC_ROW.TDS]).text());
+
+  return {
+    rush_attempts,
+    rush_yds,
+    rush_tds,
+    rec_rec,
+    rec_yds,
+    rec_tds
   };
 }
 
@@ -57,24 +104,29 @@ function parseBoxScoreHtml(html) {
     const testPlayer = commonParseRow($, $row);
     const playerKey = utils.hashPlayer(testPlayer.name, testPlayer.team);
 
-    const p = players[playerKey] || testPlayer;
+    const p = players[playerKey] || _.extend({}, testPlayer, DEFAULTS);
     players[playerKey] = p;
 
     return p;
   }
 
-  // Passing stats
-  $('#passing tbody tr').not('.thead').each((i, row) => {
-    const $row = $(row);
-
+  function addStats(newStats, $row) {
     const fbPlayer = ensurePlayer($row);
-    const newProps = parsePassingRow($, $row);
 
-    // Edit player in place
-    _.extend(fbPlayer, newProps);
-  });
+    // ensurePlayer makes sure this player is in our map, so just edit in place
+    _.extend(fbPlayer, newStats);
+  }
 
+  function parseRows(query, parser) {
+    $(query).not('.thead').each((i, row) => {
+      const $row = $(row);
+      const newStats = parser($, $row);
+      addStats(newStats, $row);
+    });
+  }
 
+  parseRows('#passing tbody tr', parsePassingRow);
+  parseRows('#rushing_and_receiving tbody tr', parseRushRecRow);
 
   return _.values(players);
 }
