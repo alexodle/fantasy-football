f49632888d4cd9e798b8f30e4e13fb3a3e2450e9
Alex Odle <aodle@palantir.com>
2015-12-23 19:55:05 -0500

    parse rosters to a single file

--- data/parseRosterHtml.js
+++ data/parseRosterHtml.js
@@ -11,7 +11,7 @@ const ROSTER_ROW = {
   CLASS: 5
 };
 
-function parseRosterHtml(html) {
+function parseRosterHtml(team, html) {
   const $ = cheerio.load(html);
   var parsed = [];
   $('tr.evenrow, tr.oddrow').each((i, row) => {
@@ -19,36 +19,35 @@ function parseRosterHtml(html) {
     const number = _.parseInt($(tds[ROSTER_ROW.NUMBER]).text());
     const name = $(tds[ROSTER_ROW.NAME]).text();
     const pos = $(tds[ROSTER_ROW.POS]).text();
-    parsed.push({ number, name, pos });
+    parsed.push({ team, number, name, pos });
   });
   return parsed;
 }
 
-function run(rosterDir, outputDir, config) {
+function run(rosterDir, outputFile, config) {
   console.log('parseRosterHtml.run:');
   console.log('\trosterDir: ' + rosterDir);
-  console.log('\toutputDir: ' + outputDir);
+  console.log('\toutputFile: ' + outputFile);
   console.log('\tconfig: ' + config);
   console.log('');
   return Promise.all(_.map(config.team_filter, (teamName) => {
     const htmlFile = path.resolve(rosterDir, utils.sanitizeFileName(teamName + '.html'));
-    return utils.readFile(htmlFile, 'utf8')
-      .then(parseRosterHtml)
-      .then(parsed => {
-        const outFileName = utils.sanitizeFileName(`${teamName}.json`);
-        const outFile = path.resolve(outputDir, outFileName);
-        const json = JSON.stringify({ data: parsed }, null, 2);
-        return utils.writeFile(outFile, json);
-      })
-      .catch(err => console.error(err));
-  }));
+    return utils
+      .readFile(htmlFile, 'utf8')
+      .then(html => parseRosterHtml(teamName, html));
+  }))
+  .then(results => {
+    const json = JSON.stringify({ data: _.flatten(results) }, null, 2);
+    return utils.writeFile(outputFile, json);
+  })
+  .catch(err => console.error(err));
 }
 
 function main() {
   const argv = require('yargs')
     .usage('Usage: $0 -d -o -c')
     .describe('d', 'roster html directory')
-    .describe('o', 'output directory')
+    .describe('o', 'output json file')
     .describe('c', 'config file')
     .demand(['d', 'o', 'c'])
     .argv;
