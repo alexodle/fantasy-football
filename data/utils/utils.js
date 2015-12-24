d82c08f6d76a28f1d797bbdfe0b584c565030bb8
Alex Odle <aodle@palantir.com>
2015-12-23 20:16:41 -0500

    comment++

--- data/utils/utils.js
+++ data/utils/utils.js
@@ -1,6 +1,7 @@
 const Promise = require('promise');
 const fs = require('fs');
 
+// Turns these functions in to promise-returners
 const readFile = Promise.denodeify(fs.readFile);
 const writeFile = Promise.denodeify(fs.writeFile);
 
