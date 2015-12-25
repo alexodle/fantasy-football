5b7c6dc4f7f221a3cfeddb0d683edf15023f37db
Alex Odle <aodle@palantir.com>
2015-12-25 10:27:20 -0800

    number the output directories so the progression is obvious

--- data/run-score-update.sh
+++ data/run-score-update.sh
@@ -16,26 +16,39 @@ date=$(date +"%m%d%Y-%H%M%S")
 
 echo "START: ${date}"
 
+# make the file progression clear by placing output files in number-ordered sub-directories
+i=0
+
 data_dir=$stash_dir/$date; mkdir -p $data_dir
-raw_dir=$data_dir/raw; mkdir $raw_dir
-parsed_dir=$data_dir/parsed; mkdir $parsed_dir
 
 # Download html file with all the box scores links
-box_score_links_html=$raw_dir/box_score_links.html
+((i++)); subdir=$data_dir/$i; mkdir $subdir
+box_score_links_html=$subdir/box_score_links.html
+echo "wget -O $box_score_links_html http://www.sports-reference.com/cfb/years/2015-schedule.html"
+echo ""
 wget -O $box_score_links_html http://www.sports-reference.com/cfb/years/2015-schedule.html
 
 pushd $BASEDIR/node_scripts
 
 # box score links html -> box score links json
-box_score_links_json=$parsed_dir/box_score_links.json
+((i++)); subdir=$data_dir/$i; mkdir $subdir
+box_score_links_json=$subdir/box_score_links.json
+echo "node ./parseBoxScoreLinksHtml.js -i $box_score_links_html -o $box_score_links_json -b $BOX_SCORE_BASE_URL"
+echo ""
 node ./parseBoxScoreLinksHtml.js -i $box_score_links_html -o $box_score_links_json -b $BOX_SCORE_BASE_URL
 
 # download box score html files
-box_score_html_dir=$raw_dir/box_score_html
+((i++)); subdir=$data_dir/$i; mkdir $subdir
+box_score_html_dir=$subdir
+echo "node ./downloadBoxScoresHtml.js -i $box_score_links_json -d $box_score_html_dir -w $week -c $config"
+echo ""
 node ./downloadBoxScoresHtml.js -i $box_score_links_json -d $box_score_html_dir -w $week -c $config
 
 # parse box scores
-fb_player_stats_json=$parsed_dir/fb_player_stats.json
+((i++)); subdir=$data_dir/$i; mkdir $subdir
+fb_player_stats_json=$subdir/fb_player_stats.json
+echo "node ./parseBoxScoreHtml.js -d $box_score_html_dir -o $fb_player_stats_json"
+echo ""
 node ./parseBoxScoreHtml.js -d $box_score_html_dir -o $fb_player_stats_json
 
 popd
@@ -45,4 +58,4 @@ popd
 popd
 
 echo "DONE: $(date +"%m%d%Y-%H%M%S")"
-echo "Contents in: ${stash_dir}"
+echo "Contents in: ${data_dir}"
