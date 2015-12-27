826e04ec24b8348031fdb6673a068809f401c2cf
Alex Odle <aodle@palantir.com>
2015-12-26 16:11:56 -0800

    handle all view routes in index handler

--- server/app/main/views.py
+++ server/app/main/views.py
@@ -2,6 +2,16 @@ from flask import render_template
 from . import main
 
 
-@main.route('/', methods=['GET', 'POST'])
-def index():
+# IMPORTANT: Keep in sync with routes.jsx
+#
+# TODO: Better way to handle trailing slashes?
+#
+@main.route('/', methods=['GET'])
+@main.route('/login', methods=['GET'])
+@main.route('/login/', methods=['GET'])
+@main.route('/draft/<draftId>', methods=['GET'])
+@main.route('/draft/<draftId>/', methods=['GET'])
+def index(draftId=None):
+    # Routes and route parameters are unused here. We rely on the client code to
+    # parse and handle view routes.
     return render_template('index.html')
