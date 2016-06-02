9ffe172a04a7ba31298a46ed4dc5c3c70e0d7faf
Alex Odle <aodle@palantir.com>
2016-06-01 22:05:27 -0400

    safely parse son from redis messages

--- node_server/server.js
+++ node_server/server.js
@@ -8,7 +8,15 @@ const io = Server(config.socketio_port, { serveClient: false });
 redisClient.on('message', function (channel, message) {
   console.log('Received redis message!');
 
-  const payload = JSON.parse(message);
+  let payload = null;
+  try {
+    payload = JSON.parse(message);
+  } catch (e) {
+    console.error(`Unable to parse message: '${message}'`);
+    console.error(e.stack);
+    return;
+  }
+
   const {league_id} = payload;
 
   // TODO - when we add more message types we will have to parse the message to
